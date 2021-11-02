649be393007da3bd72a27ead5450cc2843655c2c
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2021-11-02 15:02:27 +0500

    useState usage example

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button, Linking} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('Abdul Basit');
+  const [session, setSession] = useState({number: 6, title: 'state'});
+  const [current, setCurrent] = useState(true);
+
+  const onClickHandler = () => {
+    setName('Programming with Abdul');
+    setSession({number: 7, title: 'Style'});
+    setCurrent(false);
+  };
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Programming with Abdul</Text>
-      <Button
-        title="Go to Google Homepage"
-        onPress={() => {
-          Linking.openURL('https://www.google.com');
-        }}
-      />
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>
+        This is session number {session.number} and about {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? 'current session' : 'next session'}
+      </Text>
+      <Button title="Update State" onPress={onClickHandler} />
     </View>
   );
 };
