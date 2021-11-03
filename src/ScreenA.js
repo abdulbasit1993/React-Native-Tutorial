05b504c9284874a49202a026518ca4fefe8c4167
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2021-11-03 20:10:55 +0500

    Debugging in VS Code Example

--- src/ScreenA.js
+++ src/ScreenA.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text, Pressable} from 'react-native';
 
 export default function ScreenA({navigation, route}) {
+  const Users = [
+    {
+      id: 1,
+      name: 'User A',
+    },
+    {
+      id: 2,
+      name: 'User B',
+    },
+    {
+      id: 3,
+      name: 'User C',
+    },
+  ];
+
+  const [name, setName] = useState('');
+
   const onPressHandler = () => {
-    navigation.navigate('Screen_B');
+    // navigation.navigate('Screen_B');
     // navigation.toggleDrawer();
+    for (let user of Users) {
+      setName(user.name);
+    }
   };
 
   return (
@@ -13,9 +33,9 @@ export default function ScreenA({navigation, route}) {
       <Pressable
         onPress={onPressHandler}
         style={({pressed}) => ({backgroundColor: pressed ? '#ddd' : '#0f0'})}>
-        <Text style={styles.text}>Go to Screen B</Text>
+        <Text style={styles.text}>Get the last user</Text>
       </Pressable>
-      <Text style={styles.text}>{route.params?.Message}</Text>
+      <Text style={styles.text}>{name}</Text>
     </View>
   );
 }
