0c7b1f0f76d411a59103e8bde6ed4bf4636e29bd
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2021-11-10 18:01:43 +0500

    Added some features

--- src/screens/Task.js
+++ src/screens/Task.js
@@ -1,9 +1,18 @@
 import React, {useState, useEffect} from 'react';
-import {Alert, StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import CustomButton from '../utils/CustomButton';
 import {useSelector, useDispatch} from 'react-redux';
 import {setTasks} from '../redux/actions';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import CheckBox from '@react-native-community/checkbox';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default function Task({navigation}) {
   const {tasks, taskID} = useSelector(state => state.taskReducer);
@@ -11,6 +20,8 @@ export default function Task({navigation}) {
 
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
+  const [done, setDone] = useState(false);
+  const [color, setColor] = useState('white');
 
   useEffect(() => {
     getTask();
@@ -21,6 +32,8 @@ export default function Task({navigation}) {
     if (Task) {
       setTitle(Task.Title);
       setDesc(Task.Desc);
+      setDone(Task.Done);
+      setColor(Task.Color);
     }
   };
 
@@ -33,6 +46,8 @@ export default function Task({navigation}) {
           ID: taskID,
           Title: title,
           Desc: desc,
+          Done: done,
+          Color: color,
         };
         const index = tasks.findIndex(task => task.ID === taskID);
         let newTasks = [];
@@ -70,6 +85,48 @@ export default function Task({navigation}) {
         multiline
         onChangeText={value => setDesc(value)}
       />
+      <View style={styles.color_bar}>
+        <TouchableOpacity
+          style={styles.color_white}
+          onPress={() => {
+            setColor('white');
+          }}>
+          {color === 'white' && (
+            <FontAwesome5 name={'check'} size={25} color={'#000000'} />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.color_red}
+          onPress={() => {
+            setColor('red');
+          }}>
+          {color === 'red' && (
+            <FontAwesome5 name={'check'} size={25} color={'#000000'} />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.color_blue}
+          onPress={() => {
+            setColor('blue');
+          }}>
+          {color === 'blue' && (
+            <FontAwesome5 name={'check'} size={25} color={'#000000'} />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.color_green}
+          onPress={() => {
+            setColor('green');
+          }}>
+          {color === 'green' && (
+            <FontAwesome5 name={'check'} size={25} color={'#000000'} />
+          )}
+        </TouchableOpacity>
+      </View>
+      <View style={styles.checkbox}>
+        <CheckBox value={done} onValueChange={newValue => setDone(newValue)} />
+        <Text style={styles.text}>Is Done</Text>
+      </View>
       <CustomButton
         title="Save Task"
         color="#1eb900"
@@ -86,6 +143,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
   },
+  checkbox: {
+    flexDirection: 'row',
+    margin: 10,
+  },
   input: {
     width: '100%',
     borderWidth: 1,
@@ -96,5 +157,45 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin: 10,
     paddingHorizontal: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: '#000000',
+  },
+  color_bar: {
+    flexDirection: 'row',
+    height: 50,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#555555',
+    marginVertical: 10,
+  },
+  color_white: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  color_red: {
+    flex: 1,
+    backgroundColor: '#f28b82',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  color_blue: {
+    flex: 1,
+    backgroundColor: '#aecbfa',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  color_green: {
+    flex: 1,
+    backgroundColor: '#ccff90',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
   },
 });
