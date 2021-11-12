fa84eb7798389fd5d6013934ab539afe32b73c3d
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2021-11-12 13:04:57 +0500

    Modified the App for Release

--- ios/TaskerTodoListTests/TaskerTodoListTests.m
+++ ios/TaskerTodoListTests/TaskerTodoListTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MainAppTests : XCTestCase
+@interface TaskerTodoListTests : XCTestCase
 
 @end
 
-@implementation MainAppTests
+@implementation TaskerTodoListTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
